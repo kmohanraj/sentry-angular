f7d9d13271b5a3503e014619945ac9dbd4f81986
mohanraj kanthasamy <mohanraj.kandhasamy@crystaldelta.com>
2021-07-20 15:41:32 +0530

    sentry integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  color = 'black';
   title = 'sentry-angular';
+
+
+  changeColor() {
+    var curr = this;
+    this.color = 'red';
+    setTimeout(() =>{
+      curr.color = 'black'
+    }, 1500);
+  }
 }
